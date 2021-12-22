a9cc3633f4d86c91ad976eee653c7e1744a74d02
rksgr <r_kjha@yahoo.com>
2021-12-22 08:44:50 +0530

    Added: Welcome message

---
+++ StacknQueueBasic.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.dec_20_stack_n_queue;
+
+
+public class StacknQueueBasic {
+    public static void main(String []args){
+        System.out.println("Good Morning and Welcome to BridgeLabz!");
+    }
+}
